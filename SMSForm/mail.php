27040be18486c30468347550918ef61bb9e8afd2
Sakari Games <>
2020-04-13 20:05:28 +0200

    SMSForm
    SMSform

---
+++ SMSForm/mail.php
@@ -0,0 +1,57 @@
+<?php
+require "PHPMailer/PHPMailerAutoload.php";
+
+function smtpmailer($to, $from, $from_name, $subject, $body)
+    {
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->SMTPAuth = true; 
+ 
+        $mail->SMTPSecure = 'tls'; 
+        $mail->Host = 'sakarigames.com';
+        $mail->Port = 465;  
+        $mail->Username = 'pepijn@sakarigames.com';
+        $mail->Password = 'R0adk1ll!';   
+   
+   //   $path = 'reseller.pdf';
+   //   $mail->AddAttachment($path);
+   
+        $mail->IsHTML(true);
+        $mail->From="pepijn@sakarigames.com";
+        $mail->FromName=$from_name;
+        $mail->Sender=$from;
+        $mail->AddReplyTo($from, $from_name);
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+        $mail->AddAddress($to);
+        if(!$mail->Send())
+        {
+            $error ="Please try Later, Error Occured while Processing...";
+            return $error; 
+        }
+        else 
+        {
+            $error = "Thanks You !! Your email is sent.";  
+            return $error;
+        }
+    }
+    
+    $to   = 'pepijn@sakarigames.com';
+    $from = 'pepijn@sakarigames.com';
+    $name = 'Pepijn';
+    $subj = 'PHPMailer 5.2 testing from DomainRacer';
+    $msg = 'This is mail about testing mailing using PHP.';
+    
+    $error=smtpmailer($to,$from, $name ,$subj, $msg);
+    
+?>
+
+<html>
+    <head>
+        <title>PHPMailer 5.2 testing from DomainRacer</title>
+    </head>
+    <body style="background: black;">
+        <center><h2 style="padding-top:70px;color: white;"><?php echo $error; ?></h2></center>
+    </body>
+    
+</html>
